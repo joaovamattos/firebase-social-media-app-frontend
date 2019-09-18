6175c77760994aaa0832bbaefa04907db1a796ae
João <joaovictorvha@gmail.com>
2019-09-18 16:07:57 -0400

    form login

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,8 +11,6 @@ export class home extends Component {
     componentDidMount(){
         axios.get('/screams')
             .then(res => {
-                console.log(res.data);
-                
                 this.setState({
                     screams: res.data
                 })
@@ -22,7 +20,7 @@ export class home extends Component {
 
     render() {
         let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => <Scream scream={scream} />)
+            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
         ) : <p>Loading...</p>
         return (
             <Grid container spacing={10}>
