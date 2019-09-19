f64964094a624aeb8aca02a847a15a2b9f492d44
João <joaovictorvha@gmail.com>
2019-09-19 15:25:49 -0400

    redux setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './utils/theme';
 import jwtDecode from 'jwt-decode';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 // Components
 import Navbar from './components/Navbar';
 import AuthRoute from './components/AuthRoute';
@@ -32,16 +35,18 @@ if (token){
 function  App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <Router>
-        <Navbar/>
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={home} />
-            <AuthRoute exact path='/login' component={login} authenticated={authenticated}/>
-            <AuthRoute exact path='/signup' component={signup} authenticated={authenticated}/>
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Navbar/>
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={home} />
+              <AuthRoute exact path='/login' component={login} authenticated={authenticated}/>
+              <AuthRoute exact path='/signup' component={signup} authenticated={authenticated}/>
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     </MuiThemeProvider>
   );
 }
