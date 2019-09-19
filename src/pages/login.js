7bd6039a839f00d934e2902f0eff8d2de5c0e214
João <joaovictorvha@gmail.com>
2019-09-19 14:18:14 -0400

    signup and auth state

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,33 +11,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        width: 60,
-        margin: '20px auto'
-    },
-    pageTitle: {
-        margin: '10px auto'
-    },
-    textField: {
-        margin: '10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    },
-    progress: {
-        position: 'absolute'
-    }
-};
+const styles = (theme) => ({
+    ...theme.spreadThis
+});
 
 export class login extends Component {    
     constructor(){
@@ -61,6 +37,7 @@ export class login extends Component {
         }
         axios.post('/login', userData)
             .then(res => {
+                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
                 console.log(res.data);
                 this.setState({
                     loading: false
@@ -127,7 +104,7 @@ export class login extends Component {
                             variant="contained" 
                             color="primary" 
                             className={classes.button}
-                            disable={loading}
+                            disabled={loading}
                         >
                             Login
                             { loading && (
@@ -135,7 +112,7 @@ export class login extends Component {
                             )}
                         </Button>
                         <br />
-                        <small>dont have an account? Sign up <Link to="/signup">here</Link></small>
+                        <small>Don't have an account? Sign up <Link to="/signup">here</Link></small>
                     </form>
                 </Grid>
                 <Grid item sm/>
---
+++ src/utils/theme.js
@@ -0,0 +1,47 @@
+export default {
+    palette: {
+        primary: {
+          light: '#33c9dc',
+          main: '#00bcd4',
+          dark: '#008394',
+          contrastText: '#ffffff'
+        },
+        secondary: {
+          light: '#ff6333',
+          main: '#ff3d00',
+          dark: '#b22a00',
+          contrastText: '#ffffff'
+        }
+      },
+
+    spreadThis: {
+        typography: {
+            useNextVariants: true
+        },
+        form: {
+            textAlign: 'center'
+        },
+        image: {
+            width: 60,
+            margin: '20px auto'
+        },
+        pageTitle: {
+            margin: '10px auto'
+        },
+        textField: {
+            margin: '10px auto'
+        },
+        button: {
+            marginTop: 20,
+            position: 'relative'
+        },
+        customError: {
+            color: 'red',
+            fontSize: '0.8rem',
+            marginTop: 10
+        },
+        progress: {
+            position: 'absolute'
+        }
+    }
+}
