837bd6ba0e0836e53613fe9eb5279167956f23e2
João <joaovictorvha@gmail.com>
2019-10-02 15:02:47 -0400

    upload image

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import themeFile from './utils/theme';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./utils/theme";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 // Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { SET_AUTHENTICATED, SET_UNAUTHENTICATED } from './redux/types';
-import { logoutUser, getUserData } from './redux/actions/userActions';  
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED, SET_UNAUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
 // Components
-import Navbar from './components/Navbar';
-import AuthRoute from './components/AuthRoute';
+import Navbar from "./components/Navbar";
+import AuthRoute from "./components/AuthRoute";
 
 //Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 const theme = createMuiTheme(themeFile);
 
 const token = localStorage.FBIdToken;
-if (token){
+if (token) {
   const decodedToken = jwtDecode(token);
   console.log(decodedToken);
-  if (decodedToken.exp * 1000 < Date.now()){
+  if (decodedToken.exp * 1000 < Date.now()) {
     store.dispatch(logoutUser());
-    window.location.href = '/login';
+    window.location.href = "/login";
   } else {
     store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
   }
 }
 
-function  App() {
+function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <Provider store={store}>
         <Router>
-          <Navbar/>
-          <div className='container'>
+          <Navbar />
+          <div className="container">
             <Switch>
-              <Route exact path='/' component={home} />
-              <AuthRoute exact path='/login' component={login}/>
-              <AuthRoute exact path='/signup' component={signup}/>
+              <Route exact path="/" component={home} />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
             </Switch>
           </div>
         </Router>
